5839846d92cfbea62d53b66cc8812f033e86bf73
RarceD <akimbo170@gmail.com>
2024-07-06 12:04:38 +0200

    Add new start program test

--- ModbusConsoleApp/Program.cs
+++ ModbusConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using ModbusLib.Protocols;
 using System;
 using System.Globalization;
 using System.IO.Ports;
+using System.Threading;
 
 namespace ModbusConsoleApp
 {
@@ -16,45 +17,26 @@ namespace ModbusConsoleApp
 
         static void Main(string[] args)
         {
-
+            // ONLY CHANGE THIS PARAMS:
             string PortName = "COM10";
+            byte SlaveId = 1;
+
+            // Actual test code:
             int Baud = 9600;
             int DataBits = 8;
             StopBits StopBits = StopBits.One;
             var Parity = System.IO.Ports.Parity.None;
-            byte SlaveId = 1;
 
             _uart = new SerialPort(PortName, Baud, Parity, DataBits, StopBits);
             _uart.Open();
             _portClient = _uart.GetClient();
             _driver = new ModbusClient(new ModbusRtuCodec()) { Address = SlaveId };
 
-            var DataLength = 1;
-
-            ushort[] waterPercentage = new ushort[] { 100 };
-
-            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_A, DataLength, waterPercentage);
-            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_A, DataLength);
-
-            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_B, DataLength, waterPercentage);
-            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_B, DataLength);
-
-            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_C, DataLength, waterPercentage);
-            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_C, DataLength);
-
-            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_D, DataLength, waterPercentage);
-            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_D, DataLength);
-
-            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_E, DataLength, waterPercentage);
-            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_E, DataLength);
-
-            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_F, DataLength, waterPercentage);
-            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_F, DataLength);
-
+            RunValveProgramIrrigationTest();
+            // RunWaterPercentageTests();
+            // RunHourProgramTimeTest();
             _uart.Close();
-
             while (true) { }
-
         }
 
         private static void WriteHoldingRegister(int StartAddress, int DataLength, ushort[] data)
@@ -80,7 +62,6 @@ namespace ModbusConsoleApp
             }
         }
 
-
         private static void ReadHoldingRegister(int StartAddress, int DataLength)
         {
             var command = new ModbusCommand(ModbusCommand.FuncReadMultipleRegisters) { Offset = StartAddress, Count = DataLength, TransId = _transactionId++ };
@@ -97,5 +78,63 @@ namespace ModbusConsoleApp
                 Console.WriteLine(String.Format("Read ERROR => Function code:{0}.", result.Status));
             }
         }
+
+        public static void RunValveProgramIrrigationTest()
+        {
+        }
+
+        public static void RunHourProgramTimeTest()
+        {
+            var DataLength = 1;
+            for (ushort i = 0; i < 6; i++)
+            {
+                ushort[] timeInMinutes = new ushort[] { (ushort)(120 + i * 10) };
+                WriteHoldingRegister(MemmoryMap.MODBUS_SET_START_PROGRAM_A + i, DataLength, timeInMinutes);
+                ReadHoldingRegister(MemmoryMap.MODBUS_GET_START_PROGRAM_A + i, DataLength);
+                Thread.Sleep(200);
+
+                WriteHoldingRegister(MemmoryMap.MODBUS_SET_START_PROGRAM_B + i, DataLength, timeInMinutes);
+                ReadHoldingRegister(MemmoryMap.MODBUS_GET_START_PROGRAM_B + i, DataLength);
+
+                WriteHoldingRegister(MemmoryMap.MODBUS_SET_START_PROGRAM_C + i, DataLength, timeInMinutes);
+                ReadHoldingRegister(MemmoryMap.MODBUS_GET_START_PROGRAM_C + i, DataLength);
+                Thread.Sleep(200);
+
+                WriteHoldingRegister(MemmoryMap.MODBUS_SET_START_PROGRAM_D + i, DataLength, timeInMinutes);
+                ReadHoldingRegister(MemmoryMap.MODBUS_GET_START_PROGRAM_D + i, DataLength);
+
+                WriteHoldingRegister(MemmoryMap.MODBUS_SET_START_PROGRAM_E + i, DataLength, timeInMinutes);
+                ReadHoldingRegister(MemmoryMap.MODBUS_GET_START_PROGRAM_E + i, DataLength);
+                Thread.Sleep(200);
+
+                WriteHoldingRegister(MemmoryMap.MODBUS_SET_START_PROGRAM_F + i, DataLength, timeInMinutes);
+                ReadHoldingRegister(MemmoryMap.MODBUS_GET_START_PROGRAM_F + i, DataLength);
+            }
+        }
+
+        public static void RunWaterPercentageTest()
+        {
+            ushort[] waterPercentage = new ushort[] { 123 };
+            var DataLength = 1;
+
+            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_A, DataLength, waterPercentage);
+            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_A, DataLength);
+
+            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_B, DataLength, waterPercentage);
+            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_B, DataLength);
+
+            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_C, DataLength, waterPercentage);
+            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_C, DataLength);
+
+            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_D, DataLength, waterPercentage);
+            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_D, DataLength);
+
+            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_E, DataLength, waterPercentage);
+            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_E, DataLength);
+
+            WriteHoldingRegister(MemmoryMap.WRITE_WATER_PROGRAM_F, DataLength, waterPercentage);
+            ReadHoldingRegister(MemmoryMap.READ_WATER_PROGRAM_F, DataLength);
+        }
+
     }
 }
